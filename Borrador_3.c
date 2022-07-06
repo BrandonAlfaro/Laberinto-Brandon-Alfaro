6e49b507c1736a9b4c90be890f1ed4fa9826f3e7
BrandonAlfaro <brandonandreyalfaro@gmail.com>
2022-07-05 21:49:53 -0600

    se inicio la creacion de la funcion de busca_inicios_laberinto

--- Borrador_3.c
+++ Borrador_3.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "tamano_matriz.h"
 #include "espacio_matriz.h"
+#include "txt_a_arreglo.h"
 
 int c_filas,c_columnas;
 int espacio_de_juego [];
@@ -11,6 +12,6 @@ int main()
 {
     dimensiona_la_matriz(&c_filas,&c_columnas);
     reserva_espacio(&espacio_de_juego,c_filas,c_columnas);
-    
+    matriz_txt_a_arreglo(&espacio_de_juego);
     return 0;
 }
